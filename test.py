a830f80e078e1363910ba55dd79ac54c60c14e31
indextrown <69367698+indextrown@users.noreply.github.com>
2021-09-05 17:13:52 +0900

    Update test.py

--- test.py
+++ test.py
@@ -80,6 +80,6 @@ with open('data.js', "r", encoding="UTF-8-sig") as f:
         line = f.readline()
 #파일에 변수명을 추가하여 다시 쓴다.
 final_data = f"var data = {data};"
-final_data = f"var 시가총액 = '{시가총액}';" + final_data
+final_data = f"var 시가총액 = '{시가총액}';"\n + final_data
 with open('data.js', "w", encoding="UTF-8-sig") as f_write:
     f_write.write(final_data) 
