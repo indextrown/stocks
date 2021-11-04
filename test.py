e92c258642f9954dc3f7297a6fc588c2d6bdc567
indextrown <69367698+indextrown@users.noreply.github.com>
2021-11-04 12:31:29 +0900

    Update test.py

--- test.py
+++ test.py
@@ -85,5 +85,5 @@ final_data = f"var 시가총액 = '{시가총액}';\n\
 with open('data.js', "w", encoding="UTF-8-sig") as f_write:
     f_write.write(final_data) 
 #파일을 역순으로   
-with open('data1.js', "w", encoding="UTF-8-sig" , reverse=True) as f_write:
+with open('data1.js', "w", reverse=True) as f_write:
     f_write.write(final_data) 
